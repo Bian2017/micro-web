7a6dfb586c9459e182715929aab0c0c9cf3a8522
Bian2017 <bgl_cumt_zju@163.com>
2021-07-17 05:38:59 +0800

    ✨ feat: 完成react15微前端接入

--- react15/index.js
+++ react15/index.js
@@ -1,33 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import BasicMap from './src/router/index.jsx';
-import "./index.scss"
-import { setMain } from './src/utils/global'
+import './index.scss';
 
+/**
+ * 创建render函数
+ *
+ * 注：由于ReactDOM.render返回的并不是一个对象内容，故在unmount生命周期
+ * 中无法像Vue那样清除对象内容。后续可以将根元素节点上的内容置为空。
+ */
 const render = () => {
-  ReactDOM.render((
-    <BasicMap />
-  ), document.getElementById('app-react'))
-}
+  ReactDOM.render(<BasicMap />, document.getElementById('app-react'));
+};
 
+// 判断当前环境是否在微前端环境，不在微前端环境则直接执行
 if (!window.__MICRO_WEB__) {
-  render()
+  render();
 }
 
-export async function bootstrap() {
-  console.log('react bootstrap')
+/**
+ * 在微前端环境，则暴露一组生命周期
+ */
+export function bootstrap() {
+  console.log('react bootstrap');
 }
 
-export async function mount(app) {
-  setMain(app) // 记录主应用传过来的方法
-  console.log('react mount')
-  render()
+export function mount(app) {
+  console.log('react mount');
+  render();
 }
 
-export async function unmount(ctx) {
-  console.log('react unmout')
-  const { container } = ctx
-  if (container) {
-    document.querySelector(container).innerHTML = ''
-  }
+export function unmount(ctx) {
+  console.log('react unmout');
 }
--- react15/webpack.config.js
+++ react15/webpack.config.js
@@ -1,18 +1,32 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    path: ['./index.js']
+    path: ['./index.js'],
   },
+  /**
+   * webpack 整体打包的时候会打包成 (function(){ ....代码内容....})() 这样一个立即执行的方法，
+   * 在这个方法里有我们所有的代码内容。当配置了library之后，webpack 在打包的时候就会配置成如下格式：
+   * var react15 = (function(){})()
+   * 变量 react15 是存放在全局的，通过 react15 这个全局变量就可以拿到各个生命周期方法
+   */
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'react15.js',
-    library: 'react15',
+    /**
+     * 把子应用打包成 umd 库格式(支持commonJS，即浏览器环境、node环境都可以进行引用)
+     */
     libraryTarget: 'umd',
+    filename: 'react15.js', // 文件打包出来的名字
+    /**
+     * 可以使我们在当前全局环境下获取我们当前打包的内容，即通过 window.react15 可
+     * 以获取子应用打包的内容，在微前端框架里会用到这一信息(value值对应子应用名称)
+     */
+    library: 'react15',
+    // 会把 AMD 模块命名为 UMD 构建
     umdNamedDefine: true,
-    publicPath: 'http://localhost:9002/'
+    publicPath: 'http://localhost:9002/',
   },
   module: {
     rules: [
@@ -21,41 +35,52 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
       {
         test: /\.(c|sc)ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: {
           loader: 'url-loader',
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
   optimization: {
     splitChunks: false,
-    minimize: false
+    minimize: false,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
     }),
 
     new MiniCssExtractPlugin({
-      filename: '[name].css'
-    })
+      filename: '[name].css',
+    }),
   ],
   devServer: {
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'dist'), // 告诉服务器从哪里提供内容
+    hot: true, // 是否热更新
     compress: true,
     port: 9002,
+    headers: {
+      /**
+       * 配置本地服务的跨域，允许所有的资源可以被访问
+       *
+       * 注：为什么要配置跨域呢？
+       *
+       * 在写微前端框架的时候，其实我们需要在主应用里或者框架里去获取当前本地服务里的内容。
+       * 如果在获取的时候不去给它设置跨域，会出现资源的拦截，导致无法获取子应用中的内容，因
+       * 此需设置成允许跨域
+       */
+      'Access-Control-Allow-Origin': '*',
+    },
     historyApiFallback: true,
-    hot: true,
-  }
-}
+  },
+};

1ec35d38babae49c7811720857a9372ccf702ca8
Bian2017 <bgl_cumt_zju@163.com>
2021-07-19 06:27:00 +0800

    ✨ feat(): 完成主应用的开发

---
+++ main/src/components/Header/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- 用户信息 -->
+    <UserInfo />
+    <!-- 导航 -->
+    <MainNav />
+  </div>
+</template>
+
+<script lang="ts">
+import UserInfo from './UserInfo';
+import MainNav from './MainNav';
+
+export default {
+  name: 'header',
+  components: {
+    UserInfo,
+    MainNav,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
